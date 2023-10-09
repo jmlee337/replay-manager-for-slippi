a2bdb81f3ad74a402ae3ce98760f5c58c8e45713
jmlee337 <jmlee337@gmail.com>
2023-10-09 12:34:17 +0900

    remove template ipcs

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,31 +1,9 @@
-import {
-  contextBridge,
-  ipcRenderer,
-  IpcRendererEvent,
-  OpenDialogReturnValue,
-} from 'electron';
+import { contextBridge, ipcRenderer, OpenDialogReturnValue } from 'electron';
 import { Event, Phase, PhaseGroup, Replay, Sets } from '../common/types';
 
 export type Channels = 'ipc-example';
 
 const electronHandler = {
-  ipcRenderer: {
-    sendMessage(channel: Channels, ...args: unknown[]) {
-      ipcRenderer.send(channel, ...args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-  },
   chooseDir: (): Promise<OpenDialogReturnValue> =>
     ipcRenderer.invoke('chooseDir'),
   getReplaysInDir: (dir: string): Promise<Replay[]> =>
