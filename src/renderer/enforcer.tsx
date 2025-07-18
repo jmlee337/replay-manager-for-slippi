b81ac99ec5514419a5518151558935eaab1596d9
Nicolet <jmlee337@gmail.com>
2025-07-18 11:40:17 -0500

    filter out games with no enforcer detection

--- src/renderer/enforcer.tsx
+++ src/renderer/enforcer.tsx
@@ -11,60 +11,62 @@ window.onload = () => {
   window.electron.onEnforcer(async (event, replays, replayLoadCount) => {
     try {
       window.electron.sendEnforcerResults(
-        await Promise.all(
-          replays.map(async (replay) => {
-            const game = new SlippiGame(replay.buffer);
+        (
+          await Promise.all(
+            replays.map(async (replay) => {
+              const game = new SlippiGame(replay.buffer);
 
-            const playerFailures: EnforcePlayerFailure[] = [];
-            if (isSlpMinVersion(game)) {
-              return { fileName: replay.fileName, playerFailures };
-            }
+              const playerFailures: EnforcePlayerFailure[] = [];
+              if (isSlpMinVersion(game)) {
+                return { fileName: replay.fileName, playerFailures };
+              }
 
-            const validPorts = new Set(
-              game
-                .getSettings()
-                ?.players.filter((player) => player.type === 0)
-                .map((player) => player.port),
-            );
-            for (let port = 1; port < 5; port += 1) {
-              if (validPorts.has(port)) {
-                const playerFailure: EnforcePlayerFailure = {
-                  checkNames: [],
-                  port,
-                };
-                const mainStickCoords = getCoordListFromGame(
-                  game,
-                  port - 1,
-                  true,
-                );
-                const cStickCoords = getCoordListFromGame(
-                  game,
-                  port - 1,
-                  false,
-                );
-                for (let i = 0; i < checks.length; i += 1) {
-                  const checkName = checks[i].name;
-                  if (checkName !== 'Control Stick Visualization') {
-                    const checkResult = checks[i].checkFunction(
-                      game,
-                      port - 1,
-                      checkName !== 'Disallowed Analog C-Stick Values'
-                        ? mainStickCoords
-                        : cStickCoords,
-                    );
-                    if (checkResult.result) {
-                      playerFailure.checkNames.push(checkName);
+              const validPorts = new Set(
+                game
+                  .getSettings()
+                  ?.players.filter((player) => player.type === 0)
+                  .map((player) => player.port),
+              );
+              for (let port = 1; port < 5; port += 1) {
+                if (validPorts.has(port)) {
+                  const playerFailure: EnforcePlayerFailure = {
+                    checkNames: [],
+                    port,
+                  };
+                  const mainStickCoords = getCoordListFromGame(
+                    game,
+                    port - 1,
+                    true,
+                  );
+                  const cStickCoords = getCoordListFromGame(
+                    game,
+                    port - 1,
+                    false,
+                  );
+                  for (let i = 0; i < checks.length; i += 1) {
+                    const checkName = checks[i].name;
+                    if (checkName !== 'Control Stick Visualization') {
+                      const checkResult = checks[i].checkFunction(
+                        game,
+                        port - 1,
+                        checkName !== 'Disallowed Analog C-Stick Values'
+                          ? mainStickCoords
+                          : cStickCoords,
+                      );
+                      if (checkResult.result) {
+                        playerFailure.checkNames.push(checkName);
+                      }
                     }
                   }
-                }
-                if (playerFailure.checkNames.length > 0) {
-                  playerFailures.push(playerFailure);
+                  if (playerFailure.checkNames.length > 0) {
+                    playerFailures.push(playerFailure);
+                  }
                 }
               }
-            }
-            return { fileName: replay.fileName, playerFailures };
-          }),
-        ),
+              return { fileName: replay.fileName, playerFailures };
+            }),
+          )
+        ).filter(({ playerFailures }) => playerFailures.length > 0),
         replayLoadCount,
       );
     } catch (e: any) {
